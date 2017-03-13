8b3cfe437685c8fd002df4d4e3d441d52fc6ff71
tuvan <tuvan@DESKTOP-DS2LQAS>
2017-03-13 21:03:54 +0700

    Hello World commitment

---
+++ src/main/java/com/tutran/common/action/HelloWorldAction.java
@@ -0,0 +1,23 @@
+package com.tutran.common.action;
+
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+import org.apache.struts.action.Action;
+import org.apache.struts.action.ActionForm;
+import org.apache.struts.action.ActionForward;
+import org.apache.struts.action.ActionMapping;
+
+import com.tutran.common.form.HelloWorldForm;
+
+public class HelloWorldAction extends Action {
+	
+	@Override
+	public ActionForward execute(ActionMapping mapping, ActionForm form, HttpServletRequest request,
+			HttpServletResponse response) throws Exception {
+		HelloWorldForm hello = (HelloWorldForm) form;
+		hello.setMessage("Welcome to Struts");
+		return mapping.findForward("success");
+	}
+
+}
---
+++ src/main/java/com/tutran/common/form/HelloWorldForm.java
@@ -0,0 +1,23 @@
+package com.tutran.common.form;
+
+import org.apache.struts.action.ActionForm;
+
+public class HelloWorldForm extends ActionForm {
+	
+	private String message;
+
+	/**
+	 * @return the message
+	 */
+	public String getMessage() {
+		return message;
+	}
+
+	/**
+	 * @param message the message to set
+	 */
+	public void setMessage(String message) {
+		this.message = message;
+	}
+	
+}
